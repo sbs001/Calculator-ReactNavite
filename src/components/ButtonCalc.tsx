625b4dc5cd56634850c2dcd21811c46f30c216a3
Sebastian <tassone_af@yahoo.com>
2021-08-06 21:19:44 -0300

    Feat: button make number

--- src/components/ButtonCalc.tsx
+++ src/components/ButtonCalc.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { buttonStyles, COLOR_GREY, COLOR_LIGTH_GREY } from '../themes/ButtonTheme';
 
 interface Props {
   text: string,
   color?: string,
-  double?: boolean
+  double?: boolean,
+  action: (button:string) => void,
 }
-export default function ButtonCalc({ text, color = COLOR_LIGTH_GREY, double }: Props) {
+export default function ButtonCalc({ text, color = COLOR_LIGTH_GREY, double, action }: Props) {
   return (
-    <View style={{ ...buttonStyles.container, backgroundColor: color, width: double ? 180 : 80 }}>
-      <Text style={{ ...buttonStyles.content, color: color === COLOR_GREY ? 'black' : 'white' }}>{text}</Text>
-    </View>
+    <TouchableOpacity onPress={() => action(text)}>
+      <View style={{ ...buttonStyles.container, backgroundColor: color, width: double ? 180 : 80 }}>
+        <Text style={{ ...buttonStyles.content, color: color === COLOR_GREY ? 'black' : 'white' }} >{text}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,49 +1,61 @@
 import React from 'react';
+import { useState } from 'react';
 import { SafeAreaView, StatusBar, Text, View } from 'react-native';
 import ButtonCalc from '../components/ButtonCalc';
 import { appStyles } from '../themes/AppTheme';
 import { COLOR_GREY, COLOR_ORANGE } from '../themes/ButtonTheme';
 
 export default function CalculatorScreen() {
+
+  const [number,setNumber] = useState('');
+  const [prevNumber,setPrevNumber] = useState('');
+
+  const clean = () => {
+    setNumber('')
+  }
+  const makeNumber =  (button:string) => {
+    setNumber(number + button)
+  }
+  
   return (
     <View style={appStyles.calculatorCtn}>
       <SafeAreaView>
         <StatusBar backgroundColor='black' barStyle='light-content' />
-        <Text style={appStyles.prevResult}>calcuator</Text>
-        <Text style={appStyles.result}>calcuator</Text>
+        <Text style={appStyles.prevResult}>{prevNumber}</Text>
+        <Text style={appStyles.result} adjustsFontSizeToFit numberOfLines={1}>{number}</Text>
 
         <View style={appStyles.row}>
-          <ButtonCalc text={'C'} color={COLOR_GREY} />
-          <ButtonCalc text={'+/-'} color={COLOR_GREY} />
-          <ButtonCalc text={'DEL'} color={COLOR_GREY} />
-          <ButtonCalc text={'/'} color={COLOR_ORANGE} />
+          <ButtonCalc text={'C'} color={COLOR_GREY} action={clean}/>
+          <ButtonCalc text={'+/-'} color={COLOR_GREY} action={clean}/>
+          <ButtonCalc text={'DEL'} color={COLOR_GREY} action={clean} />
+          <ButtonCalc text={'/'} color={COLOR_ORANGE} action={clean}/>
         </View>
 
         <View style={appStyles.row}>
-          <ButtonCalc text={'7'}/>
-          <ButtonCalc text={'8'}/>
-          <ButtonCalc text={'9'}/>
-          <ButtonCalc text={'x'} color={COLOR_ORANGE} />
+          <ButtonCalc text={'7'} action={makeNumber}/>
+          <ButtonCalc text={'8'} action={makeNumber}/>
+          <ButtonCalc text={'9'} action={makeNumber}/>
+          <ButtonCalc text={'x'} color={COLOR_ORANGE} action={clean}/>
         </View>
 
         <View style={appStyles.row}>
-          <ButtonCalc text={'4'}/>
-          <ButtonCalc text={'5'}/>
-          <ButtonCalc text={'6'}/>
-          <ButtonCalc text={'-'} color={COLOR_ORANGE} />
+          <ButtonCalc text={'4'} action={makeNumber}/>
+          <ButtonCalc text={'5'} action={makeNumber}/>
+          <ButtonCalc text={'6'} action={makeNumber} />
+          <ButtonCalc text={'-'} color={COLOR_ORANGE} action={clean}/>
         </View>
 
         <View style={appStyles.row}>
-          <ButtonCalc text={'1'}/>
-          <ButtonCalc text={'2'}/>
-          <ButtonCalc text={'3'}/>
-          <ButtonCalc text={'+'} color={COLOR_ORANGE} />
+          <ButtonCalc text={'1'} action={makeNumber}/>
+          <ButtonCalc text={'2'} action={makeNumber}/>
+          <ButtonCalc text={'3'} action={makeNumber}/>
+          <ButtonCalc text={'+'} color={COLOR_ORANGE} action={clean}/>
         </View>
 
         <View style={appStyles.row}>
-          <ButtonCalc text={'0'} double/>
-          <ButtonCalc text={'.'}/>
-          <ButtonCalc text={'='} color={COLOR_ORANGE} />
+          <ButtonCalc text={'0'} double action={makeNumber}/>
+          <ButtonCalc text={'.'} action={clean}/>
+          <ButtonCalc text={'='} color={COLOR_ORANGE} action={clean}/>
         </View>
       </SafeAreaView>
     </View>
